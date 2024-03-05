e01c1f043431bee79f8361d155717dc4f648f9d4
John Lopez <john.lopez3695@gmail.com>
2024-03-05 12:30:27 -0500

    add /api route to app

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import db from "./db/connection";
+import db from "./db/connection.js";
 import cors from "cors";
 import logger from "morgan";
 import chalk from "chalk";
@@ -10,13 +10,13 @@ app.use(express.json());
 app.use(cors());
 app.use(logger("dev"));
 
-// app.use("/", routes)
+app.use("/api", routes);
 
 const PORT = process.env.PORT || 3000;
 
 db.on("connected", () => {
   console.clear();
-  console.log(chalk.cyan.bgMagentaBright("Connected to MongoDB"));
+  console.log(chalk.cyanBright.bgMagenta("Connected to MongoDB"));
 
   app.listen(PORT, () => {
     console.log(chalk.yellowBright(`Express server running on port ${PORT}`));
